512d0a59d10a97b1ed71f90ec965779d8d9d4115
Superbiebel <superbiebel@gmail.com>
2021-12-30 18:00:44 +0100

    added Checker#getAllPossibleObjects method

--- src/main/java/me/omegabiebel/zanzibar_poc/Checker.java
+++ src/main/java/me/omegabiebel/zanzibar_poc/Checker.java
@@ -1,5 +1,7 @@
 package me.omegabiebel.zanzibar_poc;
 
+import java.util.List;
+import me.omegabiebel.zanzibar_poc.interfaces.ZCalculable;
 import org.jetbrains.annotations.NotNull;
 
 public class Checker {
@@ -8,6 +10,9 @@ public class Checker {
     }
 
     public static boolean checkPermissionWithGraphTraversal(@NotNull ZPermission checkable, @NotNull ZObject actor) {
-        return checkable.calculateObjects().contains(actor);
+        return getAllPossibleObjects(checkable).contains(actor);
+    }
+    public static List<ZObject> getAllPossibleObjects(@NotNull ZCalculable calculable) {
+        return calculable.calculateObjects();
     }
 }
